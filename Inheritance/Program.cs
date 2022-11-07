23bddbc8a658028517212df852f2766d4c59bd06
SohailLuffy <sohailislammail97@gmail.com>
2022-11-07 21:13:29 +0530

    Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+public class Animal
+{
+    public void eat() 
+    { 
+        Console.WriteLine("Eating"); 
+    }
+}
+public class Dog : Animal
+{
+    public void bark() 
+    { 
+        Console.WriteLine("Barking"); 
+    }
+}
+public class Puppies : Dog
+{
+    public void weep() 
+    { 
+        Console.WriteLine("Weeping"); 
+    }
+}
+class TestInheritance
+{
+    public static void Main(string[] args)
+    {
+        Puppies d1 = new Puppies();
+        d1.eat();
+        d1.bark();
+        d1.weep();
+    }
+}
